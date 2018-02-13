16cdc9509497094344c062e1c253ecfc2f959c3f
CameronNicolson <cameronnicolson50@gmail.com>
2018-02-13 21:47:27 +0000

    Fixed Context_dict not defined

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -102,16 +102,17 @@ def index(request):
 	category_list = Category.objects.order_by('-likes')[:5] 
 	page_list = Page.objects.order_by('-views')[:5]
 
+	visitor_cookie_handler(request)
 	context_dict = {'categories': category_list, 'pages': page_list}
 	context_dict['visits'] = request.session['visits']
 
-	visitor_cookie_handler(request)
 	response = render(request, 'rango/index.html', context=context_dict)
 	return response
 
 def about(request):
 	request.session.set_test_cookie()
 	visitor_cookie_handler(request)
+	context_dict = {}
 	context_dict['visits'] = request.session['visits']
 	response = render(request, 'rango/about.html', context=context_dict)
 	return response
