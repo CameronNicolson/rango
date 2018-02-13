6ab47e0cfd89c69bb926cbe6e2bf27ccb7cdd0b1
CameronNicolson <cameronnicolson50@gmail.com>
2018-02-13 17:01:57 +0000

    Chapter 10

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -11,19 +11,27 @@ from django.contrib.auth import logout
 from datetime import datetime
 
 
-def visitor_cookie_handler(request, response):
-	visits = int(request.COOKIES.get('visits', '1'))
-	last_visit_cookie = request.COOKIES.get('last_visit', str(datetime.now()))
+def get_server_side_cookie(request, cookie, default_val=None): 
+	val = request.session.get(cookie) 
+	if not val:
+		val = default_val 
+
+	return val
+
+def visitor_cookie_handler(request):
+	visits = int(get_server_side_cookie(request, 'visits', '1'))
+	last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()))
 	last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
+
 
 	if (datetime.now() - last_visit_time).days > 0:
 		visits = visits + 1
-		response.set_cookie('last_visit', str(datetime.now()))
+		request.session['last_visit'] = str(datetime.now())
 	else:
 		visits = 1
-		response.set_cookie('last_visit', last_visit_cookie)
+		request.session['last_visit'] = last_visit_cookie
 
-	response.set_cookie('visits', visits)
+	request.session['visits'] = visits
 
 @login_required
 def user_logout(request): 
@@ -95,22 +103,18 @@ def index(request):
 	page_list = Page.objects.order_by('-views')[:5]
 
 	context_dict = {'categories': category_list, 'pages': page_list}
+	context_dict['visits'] = request.session['visits']
 
+	visitor_cookie_handler(request)
 	response = render(request, 'rango/index.html', context=context_dict)
-	visitor_cookie_handler(request, response)
-
 	return response
 
 def about(request):
-	if request.session.test_cookie_worked(): 
-		print("TEST COOKIE WORKED!") 
-		request.session.delete_test_cookie()
-
-	# prints out whether the method is a GET or a POST 
-	print(request.method)
-	# prints out the user name, if no one is logged in it prints `AnonymousUser`
-	print(request.user)
-	return render(request, 'rango/about.html')	
+	request.session.set_test_cookie()
+	visitor_cookie_handler(request)
+	context_dict['visits'] = request.session['visits']
+	response = render(request, 'rango/about.html', context=context_dict)
+	return response
 
 def show_category(request, category_name_slug):
 	context_dict = {}
--- tango_with_django_project/tango_with_django_project/settings.py
+++ tango_with_django_project/tango_with_django_project/settings.py
@@ -35,6 +35,10 @@ ALLOWED_HOSTS = []
 
 LOGIN_URL = '/rango/login/'
 
+SESSION_EXPIRE_AT_BROWSER_CLOSE = False
+
+SESSION_COOKIE_AGE = 1209600
+
 # Application definition
 
 INSTALLED_APPS = [
